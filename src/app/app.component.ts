b092b74f3e8b8a11223cb6d2451379c31f9c5641
Zyon <zyonamadeus01@gmail.com>
2021-07-15 22:15:44 -0300

    feat: adicionado MDBootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-angular-practica';
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  imagePath: string[] = ['assets/poster1.jpg', 'assets/poster2.jpg', 'assets/poster3.jpg'];
 }
